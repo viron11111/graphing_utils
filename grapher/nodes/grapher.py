dea79eb7eaf4e3abceaeca755a2687af3d074da3
viron11111 <andygrayia@gmail.com>
2016-12-03 16:41:21 -0500

    GRAPHER: added CSV writing for data, NaviGator bag files

--- grapher/nodes/grapher.py
+++ grapher/nodes/grapher.py
@@ -18,6 +18,12 @@ import encodings
 import math
 import tf, tf2_ros
 
+import csv
+import time
+from time import localtime,strftime
+import datetime
+
+
 class grapher(object):
 
 	def odom(self, data):
@@ -29,10 +35,25 @@ class grapher(object):
 			data.pose.pose.orientation.w)
 
 		euler = tf.transformations.euler_from_quaternion(quat)
-		rospy.logwarn(euler)
+		#rospy.logwarn("yaw: %f", euler[2])
+		
+		self.thruster1_file.writerow([self.time_diff, euler[0], euler[1], euler[2], data.pose.pose.position.x, data.pose.pose.position.y, data.pose.pose.position.z])
 
+		self.time_diff = time.time() - self.start_time
+
+	def create_files(self):
+		self.start_time = time.time()
+		self.time_diff = 0
+
+		date_time = strftime("%d_%m_%y_%H_%M_%S", localtime())
+		file_name1 = "/home/andy/catkin_ws/src/graphing_utils/grapher/data/yaw_%s.csv" % date_time
+		
+		self.thruster1_file = csv.writer(open(file_name1,'w'))
+		self.thruster1_file.writerow(["Time", "Roll", "Pitch", "Yaw", "X-Position", "Y-Position", "Z-Position"])
 
 	def __init__(self):
+
+		self.create_files()
 
 		rospy.Subscriber("odom", Odometry, self.odom)
 		
